f27605b10e9a82fac106244334f86e6b42fe3fb5
Luciano Ramalho <luciano@ramalho.org>
2019-10-01 19:28:10 -0300

    scan: baby step #1

--- runes/main.go
+++ runes/main.go
@@ -6,7 +6,22 @@ import (
 	"golang.org/x/text/unicode/runenames"
 )
 
-func display(words ...string) {
+type CharName struct {
+	Char rune
+	Name string
+}
+
+
+func scan(start, end rune) <-chan CharName {
+	ch := make(chan CharName)
+	go func() {
+		ch <- CharName{' ', "SPACE"}
+		close(ch)
+	}()
+	return ch
+}
+
+func report(words ...string) {
 	char := '\u2108'
 	name := runenames.Name(char)
 	fmt.Printf("%U\t%c\t%s\n", char, char, name)
@@ -16,7 +31,7 @@ func display(words ...string) {
 
 func main() {
 	if len(os.Args) > 1 {
-		display(os.Args[1:]...)
+		report(os.Args[1:]...)
 	} else {	
 		fmt.Println("Please provide one or more words to search.")
 	}
--- runes/main_test.go
+++ runes/main_test.go
@@ -1,14 +1,32 @@
 package main
 
+import "testing"
+
 func Example() {
 	main()
 	// Output:
 	// Please provide one or more words to search.
 }
 
-func Example_display() {
-	display("scruple")
+func Example_report() {
+	report("scruple")
 	// Output:
 	// U+2108	℈	SCRUPLE
 	// 1 character found
+}
+
+func Test_scan(t *testing.T) {
+	expected := CharName{'\u0020', "SPACE"}
+	count := 0
+	char:= CharName{}
+	for char = range scan('\u0000', '\u0021') {
+		count++
+	}
+	if char != expected {
+		t.Errorf("expected %q, got %q", expected, char)
+	}
+	if count != 1 {
+		t.Errorf("expected count = 1, got %d", count)
+	}
+	
 }
