2288b39d572c37fe5aac2d0bcf83b49a6299dd5b
Luciano Ramalho <luciano@ramalho.org>
2020-08-07 00:57:03 -0300

    added TWer check

--- tools/assign.py
+++ tools/assign.py
@@ -3,8 +3,11 @@ import collections
 import random
 from pprint import pprint
 from operator import attrgetter
+from functools import partial
+
 
 Person = collections.namedtuple('Person', 'monitor group name')
+
 
 def distribute(rooms, people):
     people = list(people)
@@ -13,6 +16,15 @@ def distribute(rooms, people):
     while people:
         rooms[destination].append(people.pop())
         destination = (destination + 1) % len(rooms)
+
+
+def count_members(room, group):
+    g = 0
+    for p in room:
+        if p.group == group:
+            g += 1
+    return len(room), g
+    
 
 def main():
     try:
@@ -29,16 +41,22 @@ def main():
             else:
                 learners[p.group].append(p)
 
-    # assign monitors
     number_of_rooms = 3
-    rooms = [[] for _ in range(number_of_rooms)]
-    distribute(rooms, monitors)
+    check = False
+    while not check:
+        # assign monitors
+        rooms = [[] for _ in range(number_of_rooms)]
+        random.shuffle(monitors)
+        distribute(rooms, monitors)
 
-    # assign learners
-    for _, learner_group in learners.items():
-        rooms.sort(key=len)
-        distribute(rooms, learner_group)
-
+        # assign learners
+        for group_key, learner_group in learners.items():
+            rooms.sort(key=len)
+            distribute(rooms, learner_group)
+            key = len
+    
+        check = max(count_members(r, 'TW')[1] for r in rooms) == 2
+            
     for r, room in enumerate(rooms, 1):
         print('_' * 40, 'Room', r)
         for n, p in enumerate(sorted(room, key=attrgetter('name')), 1):
