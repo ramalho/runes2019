ceffee948c1e078253f4dfc7898d79afade8ea22
Luciano Ramalho <luciano@ramalho.org>
2020-08-16 12:51:08 -0300

    runesearch: refactor index

--- web/runesearch/index.go
+++ web/runesearch/index.go
@@ -54,3 +54,27 @@ func buildIndex(RuneNames <-chan RuneName) (index Index) {
 	}
 	return index
 }
+
+// Search takes a runefinder.Index and a query; returns a matching set of runes.
+func Search(index Index, query string) (result runeset.Set) {
+	words := tokenize(strings.ToUpper(query))
+	if len(words) == 0 {
+		return runeset.Set{}
+	}
+	chars, found := index[words[0]]
+	if !found {
+		return runeset.Set{}
+	}
+	result = chars.Copy()
+	for _, word := range words[1:] {
+		chars, found := index[word]
+		if !found {
+			return runeset.Set{}
+		}
+		result.IntersectionUpdate(chars)
+		if len(result) == 0 {
+			break
+		}
+	}
+	return result
+}
--- web/runesearch/index_test.go
+++ web/runesearch/index_test.go
@@ -101,3 +101,36 @@ func Test_buildIndex_all(t *testing.T) {
 	gotSet := index["REGISTERED"]
 	assert.Equal(t, wantSet, gotSet, "REGISTERED should map only to U+00AE")
 }
+
+var fullIndex = buildIndex(scan(0, unicode.MaxRune))
+
+func TestSearch(t *testing.T) {
+	var testCases = []struct {
+		query string
+		want  runeset.Set
+	}{
+		{"Registered", runeset.Make('®')},
+		{"ORDINAL", runeset.Make('ª', 'º')},
+		{"fraction eighths", runeset.Make('⅜', '⅝', '⅞')},
+		{"fraction eighths bang", runeset.Set{}},
+		{"fraction eighths five", runeset.Make('⅝')},
+		{"NoSuchRune", runeset.Set{}},
+		{"", runeset.Set{}},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.query, func(t *testing.T) {
+			got := Search(fullIndex, tc.query)
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestSearch_hyphenatedQuery(t *testing.T) {
+	query := "HYPHEN-MINUS"
+	want := '-'
+	got := Search(fullIndex, query)
+	if !got.Contains(want) {
+		t.Errorf("query: %q\t%q absent, got: %v",
+			query, want, got)
+	}
+}
